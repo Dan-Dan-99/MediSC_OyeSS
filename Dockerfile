381d707572c2bf4d034b0158f6a612dfa9ae6625
Daehong Kang <rkdeoghd99@hufs.ac.kr>
2025-09-02 03:03:22 +0900

    [ADD] 250901 : submit

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,29 @@
-FROM pytorch/pytorch:2.1.0-cuda11.8-cudnn8-runtime
+FROM pytorch/pytorch:2.4.0-cuda11.8-cudnn9-devel
 
 RUN apt-get update -y && \
     apt-get upgrade -y && \
-    apt-get install vim wget curl git gcc-8 g++-8 -y
+    apt-get install vim wget curl ninja-build libglib2.0-0 libsm6 libxrender-dev libxext6 libgl1-mesa-glx git gcc-8 g++-8 -y && \
+    apt-get clean
 
-RUN mkdir OyeSS
+RUN mkdir input output
 
-WORKDIR /OyeSS
 
-COPY requirements.txt .
+RUN git clone https://github.com/amuck667/TrackEval.git
+WORKDIR TrackEval
+RUN git switch devel-kp \
+    mkdir data data/gt data/trackers
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN git clone https://github.com/Dan-Dan-99/MediSC_OyeSS.git
+WORKDIR ../MediSC_OyeSS
+RUN git switch MMPose
+
+RUN pip install torch==2.4.1 torchvision==2.4.1 torchaudio==0.19.1 --index-url https://download.pytorch.org/whl/cu118
+RUN pip install -U mmengine
+RUN mim install mmcv==2.1.0 mmdet>=3.1.0
+RUN pip install -r requirements.txt
+RUN pip install -v -e .
+
+RUN mv Process_SkillEval.sh /usr/local/bin/Process_SkillEval.sh
 
 ENTRYPOINT ["tail", "-f", "/dev/null"]
+# RUN ["bash", "/usr/local/bin/Process_SkillEval.sh"]
---
+++ Process_SkillEval.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+IMAGE_DIR="/input"
+
+process_image()
+{
+    local image_file=$1
+
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /MediSC_OyeSS/configs/best_coco_bbox_mAP_epoch_100_b16.pth /MediSC_OyeSS/configs/hand_pose_m.py /MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 0 --save-predictions --output-root /MediSC_OyeSS/results/0_left_hand
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /MediSC_OyeSS/configs/best_coco_bbox_mAP_epoch_100_b16.pth /MediSC_OyeSS/configs/hand_pose_m.py /MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 1 --save-predictions --output-root /MediSC_OyeSS/results/1_right_hand
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_tools.py /MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /MediSC_OyeSS/configs/tool_pose.py /MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 0 --save-predictions --output-root /MediSC_OyeSS/results/2_scissors
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_tools.py /MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /MediSC_OyeSS/configs/tool_pose.py /MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 1 --save-predictions --output-root /MediSC_OyeSS/results/3_tweezers
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_tools.py /MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /MediSC_OyeSS/configs/tool_pose.py /MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 2 --save-predictions --output-root /MediSC_OyeSS/results/4_needle_holder
+    python /MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /MediSC_OyeSS/configs/Co-DETR_tools.py /MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /MediSC_OyeSS/configs/tool_pose.py /MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 3 --save-predictions --output-root /MediSC_OyeSS/results/5_needle
+}
+
+for image_file in "$IMAGE_DIR"/*.png; do
+    if [ -f "$image_file" ]; then
+        process_image "$image_file"
+    fi
+done
+
+python json_to_mot.py
+
+python /TrackEval/scripts/run_mot_challenge_kp.py
+
+mv /TrackEval/data/out/* /output
