1b82c3fd8e611120dcd2b1b22fc0d2f294bbc746
Daehong Kang <rkdeoghd99@hufs.ac.kr>
2025-09-02 08:09:53 +0900

    [ADD] 250901 : Submit - 6

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,20 @@
 FROM pytorch/pytorch:2.4.0-cuda11.8-cudnn9-devel
 
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=Asia/Seoul
+
+RUN apt-get update -y && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:deadsnakes/ppa && \
+    apt-get update -y && \
+    apt-get install -y python3.8 python3.8-dev python3.8-distutils python3.8-venv
+
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1 && \
+    update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
+RUN python3.8 -m ensurepip --upgrade && \
+    python3.8 -m pip install --upgrade pip
+
 RUN apt-get update -y && \
     apt-get upgrade -y && \
     apt-get install vim wget curl ninja-build libglib2.0-0 libsm6 libxrender-dev libxext6 libgl1-mesa-glx git -y && \
@@ -19,13 +34,14 @@ RUN pip install openmim
 RUN mim install -U mmengine mmcv==2.1.0 mmdet>=3.1.0
 RUN pip install -r requirements.txt
 RUN mim install mmpose>=1.1.0
+RUN pip install numpy==1.26.4 --force-reinstall
 
 
 RUN pip install gdown
 ARG GDRIVE_FOLDER_URL="https://drive.google.com/drive/folders/16bQw6r2m9EUsMAh5wwy8d9GuM4NL9BSI?usp=sharing"
 RUN gdown --folder --fuzzy "$GDRIVE_FOLDER_URL" -O /workspace/MediSC_OyeSS/configs
 
-RUN mv Process_skillEval.sh /usr/local/bin/Process_skillEval.sh
+RUN cp Process_skillEval.sh /usr/local/bin/Process_skillEval.sh
 RUN chmod +777 /usr/local/bin/Process_skillEval.sh
 
 # ENTRYPOINT ["tail", "-f", "/dev/null"]
--- Process_skillEval.sh
+++ Process_skillEval.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 IMAGE_DIR="/input"
-mv /output/gt/* /workspace/TrackEval/data/gt
+cp /output/gt/* /workspace/TrackEval/data/gt
 
 process_image()
 {
     local image_file=$1
 
-    python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /workspace/MediSC_OyeSS/configs/best_coco_bbox_mAP_epoch_100_b16.pth /workspace/MediSC_OyeSS/configs/hand_pose_m.py /workspace/MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 0 --save-predictions --output-root /workspace/MediSC_OyeSS/results/0_left_hand
-    python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /workspace/MediSC_OyeSS/configs/best_coco_bbox_mAP_epoch_100_b16.pth /workspace/MediSC_OyeSS/configs/hand_pose_m.py /workspace/MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 1 --save-predictions --output-root /workspace/MediSC_OyeSS/results/1_right_hand
+    python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /workspace/MediSC_OyeSS/configs/hand_best_coco_bbox_mAP_epoch_100_b16.pth /workspace/MediSC_OyeSS/configs/hand_pose_m.py /workspace/MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 0 --save-predictions --output-root /workspace/MediSC_OyeSS/results/0_left_hand
+    python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_hands_plz_copy.py /workspace/MediSC_OyeSS/configs/hand_best_coco_bbox_mAP_epoch_100_b16.pth /workspace/MediSC_OyeSS/configs/hand_pose_m.py /workspace/MediSC_OyeSS/configs/hand_pose_best_coco_AP_epoch_190.pth --input "$image_file" --det-cat-id 1 --save-predictions --output-root /workspace/MediSC_OyeSS/results/1_right_hand
     python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_tools.py /workspace/MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /workspace/MediSC_OyeSS/configs/tool_pose.py /workspace/MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 0 --save-predictions --output-root /workspace/MediSC_OyeSS/results/2_scissors
     python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_tools.py /workspace/MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /workspace/MediSC_OyeSS/configs/tool_pose.py /workspace/MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 1 --save-predictions --output-root /workspace/MediSC_OyeSS/results/3_tweezers
     python /workspace/MediSC_OyeSS/demo/topdown_demo_with_mmdet.py /workspace/MediSC_OyeSS/configs/Co-DETR_tools.py /workspace/MediSC_OyeSS/configs/tool_best_coco_bbox_mAP_epoch_170.pth /workspace/MediSC_OyeSS/configs/tool_pose.py /workspace/MediSC_OyeSS/configs/tool_best_epoch_250.pth --input "$image_file" --det-cat-id 2 --save-predictions --output-root /workspace/MediSC_OyeSS/results/4_needle_holder
@@ -25,4 +25,4 @@ python /workspace/MediSC_OyeSS/json_to_mot.py
 
 python /workspace/TrackEval/scripts/run_mot_challenge_kp.py
 
-mv /workspace/TrackEval/data/out/* /output
+cp /workspace/TrackEval/data/out/* /output
